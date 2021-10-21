213f32695b6de09564a6275abf32113e0742e12c
FilipCeder <cederblad.filip@gmail.com>
2021-10-21 09:06:20 +0200

    structure_summarize

--- AddpromptstructurestoJSONfile.py
+++ AddpromptstructurestoJSONfile.py
@@ -1,18 +1,18 @@
 import json
 
-filename = "Datasets\sentimentdata.json"
+filename = "Datasets\summarizedataset.json"
 # python object(dictionary) to be dumped
 prompt_structure:dict = {
-        "part1": ('Write ', 'Make ', 'This is ', 'Create ', 'Show me ', 'Display ', 'Print ', 'Reveal ', 'Reaveal to me ', 'Get me ', 'May I a have ', 'Could i have ', 'Can you give to me ', 'Please show me '),
-        "part2": {
-            "sadness": ('a sad', 'an unhappy', 'a depressed', 'a sorrowful', 'a', 'a not so happy', 'an unglad', 'a not glad', 'melancholic'),
-            "joy": ('a happy', 'a joyful', 'a glad', 'a delightful', 'a', 'a gleefull'),
-            "love": ('a', ),
-            "anger": ('a sad', 'an unhappy', 'a depressed', 'a sorrowful', 'a', 'a not so happy', 'an unglad', 'a not glad', 'melancholic'),
-            "fear": ('a scarred', 'a fearful', 'a'),
-            "surprise": ('a surpised', 'unexpected', 'a'),
-        },
-        "part3": (' sentence', ' statement', ' piece of text', ' quote', ' citation')
+        "part1": ('Summarize ', 'Compress ', 'Condense ', 'summarize ', 'compress ', 'condense ')
+        # "part2": {
+        #     "sadness": ('a sad', 'an unhappy', 'a depressed', 'a sorrowful', 'a', 'a not so happy', 'an unglad', 'a not glad', 'melancholic'),
+        #     "joy": ('a happy', 'a joyful', 'a glad', 'a delightful', 'a', 'a gleefull'),
+        #     "love": ('a', ),
+        #     "anger": ('a sad', 'an unhappy', 'a depressed', 'a sorrowful', 'a', 'a not so happy', 'an unglad', 'a not glad', 'melancholic'),
+        #     "fear": ('a scarred', 'a fearful', 'a'),
+        #     "surprise": ('a surpised', 'unexpected', 'a'),
+        # },
+        # "part3": (' sentence', ' statement', ' piece of text', ' quote', ' citation')
     }
 
 # the json file where the output must be stored
