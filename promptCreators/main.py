c79c4b0509ceed6088804cb32c1a01ab192936e8
EliasGuedra <eliasguedra@icloud.com>
2021-10-21 12:47:23 +0200

    review

--- promptCreators/main.py
+++ promptCreators/main.py
@@ -98,7 +98,7 @@ def generatePrompts(nrOfExamples, nrOfPrompts, path):
             print('\n------')
 
 
-generatePrompts(nrOfExamples=1,nrOfPrompts=1,path='Datasets/goEmotionsDataset.json')
+generatePrompts(nrOfExamples=1,nrOfPrompts=1,path='Datasets/app_reviews.json')
 # generatePrompts(nrOfExamples=1,nrOfPrompts=1,path='Datasets/summarizedataset.json')
 # generatePrompts(nrOfExamples=1,nrOfPrompts=1,path='Datasets/quest.json')
 
