6595b861c0796f7386adcaeeb16e08b1a13776c6
aiswejoakim <90309369+aiswejoakim@users.noreply.github.com>
2021-10-22 10:22:32 +0200

    imdb

--- AddpromptstructurestoJSONfile.py
+++ AddpromptstructurestoJSONfile.py
@@ -1,33 +1,14 @@
 import json
 
-filename = "Datasets\goEmotionsDataset.json"
+filename = "Datasets\imdbdataset.json"
 # python object(dictionary) to be dumped
 prompt_structure:dict = {
         "part1": ("Write ", "Make ", "This is ", "Create ", "Show me ", "Display ", "Print ", "Reveal ", "Reaveal to me ", "Get me ", "May I a have ", "Could i have ", "Can you give to me ", "Please show me "),
         "part2": {
-            "neutral": ('a neutral', 'an unbiased', 'an objective', 'an impartial', 'a'),
-            "annoyance": ('a annoying', 'a irritating', 'an infuriating', 'a tiresome'),
-            "gratitude": ('an appriciative', 'a recognizing'),
-            "anger": ('a sad', 'an unhappy', 'a depressed', 'a sorrowful', 'a', 'a not so happy', 'an unglad', 'a not glad', 'melancholic'),
-            "fear": ('a scarred', 'a fearful', 'a'),
-            "surprise": ('a surpised', 'unexpected'),
-            "desire": ('a desiring', 'a longing', 'a craving'),
-            "admiration": ('an admiring', 'a praising', 'a applauding'),
-            "confusion": ('a confusing', 'a wierd'),
-            "amusement": ('a amusing', 'a gleeful', 'a delighting'),
-            "caring": ('a caring', 'a kind', 'a tender'),
-            "embarrassment": ('an emparrasing', 'a humiliating', 'a shamefull'),
-            "grief": ('a grieving', 'a sad', 'a melancholic'),
-            "joy": ("a happy", "a joyful", "a glad", "a delightful", "a", "a gleefull"),
-            "curiosity": ("a curious", "an interesting"),
-            "disapproval": ("a disapproving", "an objecting"),
-            "optimism": ("an optimistic", "a positive"),
-            "excitement": ("an exciting", "an enthusiastic"),
-            "love": ("a loving", "an intimate"),
-            "relief": ("a relieving", "a comforting"),
-            "sadness": ("a sad", "an unhappy", "a depressed", "a sorrowful", "a", "a not so happy", "an unglad", "a not glad", "melancholic")
+            "Positive": ('a glad ', 'an happy ', 'a positive ', 'a fantastic', 'an awesome', 'a contment', 'a satisfied', 'a pleased'),
+            "Negative": ('an annoying ', 'an irritating ', 'an infuriating ', 'a tiresome', 'a terrible', 'a horrible', 'a negative', 'a bad'),
         },
-        "part3": (' sentence', ' statement', ' piece of text', ' quote', ' citation')
+        "part3": (' movie review', ' review of the movie', ' movie analysis', ' analysis of the movie', ' citation of the movie', 'movie judgement', 'judgement of the movie')
     }
 
 # the json file where the output must be stored
